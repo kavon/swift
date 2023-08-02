d020cda288a1df43486370735b7c1e45443ccdb2
Kavon Farvardin <kfarvardin@apple.com>
2023-08-02 13:51:41 -0700

    add a LayoutProtocol.cpp/h

---
+++ lib/Sema/LayoutProtocol.h
@@ -0,0 +1,24 @@
+//===--- LayoutProtocol.h - Utilities for Layout Protocols ------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides type checking utilities for @_layout protocols.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_LIB_SEMA_LAYOUTPROTOCOL_H
+#define SWIFT_LIB_SEMA_LAYOUTPROTOCOL_H
+
+namespace swift {
+
+}
+
+#endif //SWIFT_LIB_SEMA_LAYOUTPROTOCOL_H
